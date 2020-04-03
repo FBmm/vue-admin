6f63c97c75503a65dc63bb3fa6b1010bcaf5cc9f
fbm <wuq_xd@163.com>
2020-04-03 19:52:01 +0800

    sync修饰符对一个 prop 进行“双向绑定”

---
+++ src/views/vue/sync-modifier/sync-demo/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <input type="text" :value="value" @input="onInput"/>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    title: String,
+  },
+  watch: {
+    value(val) {
+      console.log(val);
+    }
+  },
+  methods: {
+    onInput(e) {
+      this.$emit('update:value', e.target.value)
+    }
+  }
+}
+</script>
