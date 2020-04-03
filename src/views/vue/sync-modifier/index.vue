b21665ec21317424497e40d692e858a4cab346ee
fbm <wuq_xd@163.com>
2020-04-03 19:50:03 +0800

    sync修饰符主组件

---
+++ src/views/vue/sync-modifier/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app-container">
+    <h3>1. prop属性双向绑定</h3>
+    <div>输入值1：{{demo1}}</div>
+    <div>input: <input type="text" v-model="demo1"></div>
+    <div>组件：<MyInput :value="demo1" @input="onChange"></MyInput></div>
+    <h3>2. .sync修饰符</h3>
+    <div>输入值2：{{demo2}}</div>
+    <SyncDemo :value.sync="demo2"></SyncDemo>(.sync修饰符对一个 prop 进行“双向绑定”)
+    <h3>3. .sync修饰符双向绑定多个props</h3>
+    <div>{{ people }}</div>
+    <PropsSync v-bind.sync="people"></PropsSync>(.sync修饰符对多个 prop 进行“双向绑定”)
+  </div>
+</template>
+
+<script>
+import MyInput from './my-input'
+import SyncDemo from './sync-demo'
+import PropsSync from './props-sync'
+export default {
+  components: { MyInput, SyncDemo, PropsSync },
+  data() {
+    return {
+      demo1: '',
+      demo2: '',
+      people: {
+        name: 'wuqian',
+        age: 26
+      },
+    }
+  },
+  methods: {
+    onChange(val) {
+      console.log(val)
+      this.demo1 = val;
+    }
+  }
+}
+</script>
