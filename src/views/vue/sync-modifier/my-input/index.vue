317590e4c16801caef6593906b6c7bb4707a23dc
fbm <wuq_xd@163.com>
2020-04-03 19:51:12 +0800

    prop属性双向绑定demo组件

---
+++ src/views/vue/sync-modifier/my-input/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <input type="text" :value="value" @input="(e) => this.$emit('input', e.target.value)"/>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    title: String,
+  },
+  watch: {
+    value(val) {
+      console.log(val);
+    }
+  }
+}
+</script>
