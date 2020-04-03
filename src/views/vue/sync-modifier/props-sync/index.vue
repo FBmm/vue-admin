34d805e545ab92553fbe81c9959de95368ea09d7
fbm <wuq_xd@163.com>
2020-04-03 19:53:11 +0800

    sync修饰符对多个 prop 进行“双向绑定”

---
+++ src/views/vue/sync-modifier/props-sync/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>姓名：<input type="text" :value="name" @input="onInputName"/></p> 
+    <p>年龄：<input type="number" :value="age" @input="onInputAge"/></p> 
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    age: Number,
+  },
+  computed: {
+    people() {
+      return `${this.name}: ${this.age}`
+    }
+  },
+  methods: {
+    onInputName(e) {
+      this.$emit('update:name', e.target.value)
+    },
+    onInputAge(e) {
+      this.$emit('update:age', e.target.value)
+    }
+  }
+}
+</script>
