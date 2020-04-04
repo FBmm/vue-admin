d9fd22f11faacc99df6f8eccfe12d64181673488
fbm <wuq_xd@163.com>
2020-04-04 20:08:40 +0800

    eslint调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -168,7 +168,7 @@ module.exports = {
       'after': true
     }],
     'space-before-blocks': [2, 'always'],
-    'space-before-function-paren': [2, 'never'],
+    // 'space-before-function-paren': [, 'never'],
     'space-in-parens': [2, 'never'],
     'space-infix-ops': 2,
     'space-unary-ops': [2, {
