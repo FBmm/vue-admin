5e22de0a2a9409374b7ec985c6d51d79fc5817f1
fbm <wuq_xd@163.com>
2020-04-08 13:17:25 +0800

    vue 插件学习

--- src/views/vue/plugin/index.vue
+++ src/views/vue/plugin/index.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="vue-container">
     <el-row>
-      <h3>1. 自定义指令 v-focus</h3>
-      <input type="text" v-focus>
+      <h3>插件</h3>
+      <p>1. Vue.js 的插件应该暴露一个 install 方法。这个方法的第一个参数是 Vue 构造器，第二个参数是一个可选的选项对象</p>
+      <pre>
+        const MyPlugin = function (Vue, options) {
+
+          // 在 vue 实例上扩展
+
+        }
+      </pre>
+      <p>2. 通过全局方法 Vue.use() 使用插件。它需要在你调用 new Vue() 启动应用之前完成</p>
+      <pre>
+        // 调用 `MyPlugin.install(Vue)`
+
+        Vue.use(MyPlugin)
+      </pre>
     </el-row>
   </div>
 </template>
@@ -11,8 +24,6 @@
 export default {
   data() {
     return {
-      value: '这是vm.data的值',
-      direction: 'left'
     }
   }
 }
