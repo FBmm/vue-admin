95034a5b462905f95ae0355616e7ef84a12d1572
fbm <wuq_xd@163.com>
2020-04-10 13:26:57 +0800

    【公共组件】两栏布局 - flex 实现

---
+++ src/components/TwoColsLayout/flex.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="two-cols-layout">
+    <div class="col__left" v-if="colLeftExpanded">
+      <slot name="left"></slot>
+    </div>
+    <div class="division_middle">
+      <div class="switch-btn" @click="toggleCol"></div>
+    </div>
+    <div class="col__right" :style="{'width': colRightWidth}">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+//
+export default {
+  name: 'TwoColsLayout',
+  data() {
+    return {
+      colLeftExpanded: true,
+    }
+  },
+  computed: {
+    colRightWidth() {
+      return this.colLeftExpanded ? 'calc(100% - 240px - 10px)' : 'calc(100% - 10px)';
+    }
+  },
+  methods: {
+    toggleCol() {
+      this.colLeftExpanded = !this.colLeftExpanded;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.two-cols-layout {
+  height: calc(100vh - 61px);
+  .col__left {
+    height: 100%;
+    width: 240px;
+    float: left;
+    background-color: #333;
+  }
+  .division_middle {
+    height: 100%;
+    background-color: #f2f2f2;
+    width: 10px;
+    float: left;
+    .switch-btn {
+      position: absolute;
+      top: 50%;
+      margin-top: -10px;
+      width: 10px;
+      height: 20px;
+      background-color: red;
+    }
+  }
+  .col__right {
+    width: calc(100% - 240px - 10px);
+    height: 100%;
+    float: right;
+    background-color: #666;
+  }
+}
+</style>
